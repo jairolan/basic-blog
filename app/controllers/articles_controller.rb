8ac1083ee7e357ed4d8abdb49c2ef012f3bc48bf
jairo <olan.mayo05@gmail.com>
2022-12-07 20:32:49 -0600

    add show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(10)
+  end
   def new
     @article = Article.new
     @article.title = 'Demo'
   end
 
   def create
-    @article = Article.create(title: params[:article][:title])
+    @article = Article.create(title: params[:article][:title], content: params[:article][:content])
     render json: @article
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/bienvenida", to: "home#index"
+  get "articles/10", to: "articles#show"
   get "articles/new", to: "articles#new"
   post "articles", to: "articles#create"
+
 end
