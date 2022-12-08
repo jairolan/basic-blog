a835f318fc99076eed4e99394035bb53c63b6e1b
jairo <olan.mayo05@gmail.com>
2022-12-08 10:21:25 -0600

    add destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/bienvenida", to: "home#index"
+  root to: "home#index"
   get "articles/:id", to: "articles#show"
   get "articles/:id/edit", to: "articles#edit"
   get "articles/new", to: "articles#new"
   post "articles", to: "articles#create"
   patch "/articles/:id", to: "articles#update", as: :article
+  delete "articles/:id", to: "articles#destroy"
 
 end
