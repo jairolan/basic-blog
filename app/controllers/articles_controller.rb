a40c60fb5d0db3e8398a480b3dff66252ba3515d
jairo <olan.mayo05@gmail.com>
2022-12-20 17:29:00 -0600

    add articles index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
-  before_action :find_params, except: [:new, :create]
+  before_action :find_params, except: [:new, :create, :index]
+
+  def index
+    @articles = Article.all
+  end
   def show
     # @article = Article.find(params[:id])
   end
